5ac9fbd0e2a9b235eb56d6bc1dcc67230a947aa2
hoamx <hoa.mx@kiotviet.com>
2024-04-20 08:11:38 +0700

    chore: add Mongoose module

--- apps/salary/src/salary.module.ts
+++ apps/salary/src/salary.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { SalaryController } from './salary.controller';
 import { SalaryService } from './salary.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { DatabaseModule, User, UserSchema } from '@app/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+      envFilePath: './apps/orders/.env',
+    }),
+    DatabaseModule,
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema,
+      },
+    ]),
+  ],
   controllers: [SalaryController],
   providers: [SalaryService],
 })
