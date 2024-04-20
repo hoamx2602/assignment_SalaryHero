d89d7757c1e28d46289e3777a4bff412a069af7f
hoamx <hoa.mx@kiotviet.com>
2024-04-20 14:07:42 +0700

    feat: add rabbitmq module to consumer app

--- apps/consumer/src/consumer.module.ts
+++ apps/consumer/src/consumer.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConsumerController } from './consumer.controller';
 import { ConsumerService } from './consumer.service';
+import { RabbitMQModule } from '@app/common';
+import * as Joi from 'joi';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        RABBIT_MQ_URI: Joi.string().required(),
+        RABBIT_MQ_SALARY_QUEUE: Joi.string().required(),
+      }),
+    }),
+    RabbitMQModule,
+  ],
   controllers: [ConsumerController],
   providers: [ConsumerService],
 })
--- apps/consumer/src/main.ts
+++ apps/consumer/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { ConsumerModule } from './consumer.module';
+import { RabbitMQService } from '@app/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(ConsumerModule);
-  await app.listen(3000);
+  const rabbitMQService = app.get<RabbitMQService>(RabbitMQService);
+  app.connectMicroservice(rabbitMQService.getOptions('SALARY'));
+  await app.startAllMicroservices();
 }
 bootstrap();
